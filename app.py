ba3070d3fc04373bc4b8026bc2c2076b37db86d5
Thiago Barros <thg.sbarros@gmail.com>
2020-10-04 15:52:47 -0300

    Adicionado conexão com o Mongo DB.

--- app.py
+++ app.py
@@ -1,9 +1,18 @@
 import os
 from flask import Flask
+from flask_pymongo import PyMongo
 from src.controller.prescription import routes
 
 application = Flask(__name__)
 application.register_blueprint(routes)
+
+application.config["MONGO_URI"] = 'mongodb://' + os.environ['MONGODB_USERNAME'] + ':' \
+                                  + os.environ['MONGODB_PASSWORD'] + '@' \
+                                  + os.environ['MONGODB_HOSTNAME'] + ':27017/' \
+                                  + os.environ['MONGODB_DATABASE']
+
+mongo = PyMongo(application)
+db = mongo.db
 
 if __name__ == '__main__':
     ENVIRONMENT_DEBUG = os.environ.get("APP_DEBUG", True)
