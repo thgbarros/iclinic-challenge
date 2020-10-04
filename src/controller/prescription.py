0f4fd4a029b4e5408d4e6e51645cd9b6c50451ae
Thiago Barros <thg.sbarros@gmail.com>
2020-10-04 14:59:57 -0300

    Implementação do controller de prescrição.

--- src/controller/prescription.py
+++ src/controller/prescription.py
@@ -1,8 +1,45 @@
-from flask import Blueprint, jsonify
+import json
+from json.decoder import JSONDecodeError
+
+from flask import Blueprint, request, jsonify
 
 routes = Blueprint('prescriptions', __name__)
 
+MALFORMED_REQUEST_ERROR = 1
+PHYSICIAN_NOT_FOUND = 2
+PATIENT_NOT_FOUND = 3
+CLINIC_NOT_FOUND = 7
+PRESCRIPTION_NOT_FOUND = 8
+
+errors = {
+    MALFORMED_REQUEST_ERROR: {"code": "01", "message": "malformed request"},
+    PHYSICIAN_NOT_FOUND: {"code": "02", "message": "physician not found"},
+    PATIENT_NOT_FOUND: {"code": "03", "message": "patient not found"},
+    CLINIC_NOT_FOUND: {"code": "07", "message": "clinic not found"},
+    PRESCRIPTION_NOT_FOUND: {"code": "08", "message": "prescription not found"}
+}
+
+
+def validate_prescription(presciption: dict):
+    if not presciption.get('clinic'): return create_error(errors.get(CLINIC_NOT_FOUND))
+    if not presciption.get('physician'): return create_error(errors.get(PHYSICIAN_NOT_FOUND))
+    if not presciption.get('patient'): return create_error(errors.get(PATIENT_NOT_FOUND))
+    if not presciption.get('text'): return create_error(errors.get(PRESCRIPTION_NOT_FOUND))
+    return None
+
+
+def create_error(error: dict) -> dict:
+    return {"error": error}
+
 
 @routes.route('/prescriptions', methods=['POST'])
-def patient():
+def prescriptions():
+    try:
+        prescription = json.loads(request.data.decode('utf8'))
+        error = validate_prescription(prescription)
+        if error: return error, 428
+
+    except JSONDecodeError:
+        return create_error(errors.get(MALFORMED_REQUEST_ERROR)), 400
+
     return jsonify(status="Ok")
--- tests/test_prescriptions.py
+++ tests/test_prescriptions.py
@@ -1,3 +1,87 @@
-def test_prescriptions(client):
+import json
+
+
+def test_json_none(client):
     response = client.post('/prescriptions')
+    error = json.loads(response.data)
+    assert error.get("error") is not None
+    error = error.get("error")
+
+    assert error.get("code") == "01"
+    assert error.get("message") == "malformed request"
+    assert response.status_code == 400
+
+
+def test_without_prescription(client):
+    response = client.post('/prescriptions', data=json.dumps({
+        "clinic": {"id": 1},
+        "physician": {"id": 1},
+        "patient": {"id": 1},
+    }))
+
+    error = json.loads(response.data)
+    assert error.get("error") is not None
+    error = error.get("error")
+
+    assert error.get("code") == "08"
+    assert error.get("message") == "prescription not found"
+    assert response.status_code == 428
+
+
+def test_without_physician(client):
+    response = client.post('/prescriptions', data=json.dumps({
+        "clinic": {"id": 1},
+        "patient": {"id": 1},
+        "text": "Dipirona 1x ao dia"
+    }))
+
+    error = json.loads(response.data)
+    assert error.get("error") is not None
+    error = error.get("error")
+
+    assert error.get("code") == "02"
+    assert error.get("message") == "physician not found"
+    assert response.status_code == 428
+
+
+def test_without_patient(client):
+    response = client.post('/prescriptions', data=json.dumps({
+        "clinic": {"id": 1},
+        "physician": {"id": 1},
+        "text": "Dipirona 1x ao dia"
+    }))
+
+    error = json.loads(response.data)
+    assert error.get("error") is not None
+    error = error.get("error")
+
+    assert error.get("code") == "03"
+    assert error.get("message") == "patient not found"
+    assert response.status_code == 428
+
+
+def test_without_clinic(client):
+    response = client.post('/prescriptions', data=json.dumps({
+        "physician": {"id": 1},
+        "patient": {"id": 1},
+        "text": "Dipirona 1x ao dia"
+    }))
+
+    error = json.loads(response.data)
+    assert error.get("error") is not None
+    error = error.get("error")
+
+    assert error.get("code") == "07"
+    assert error.get("message") == "clinic not found"
+    assert response.status_code == 428
+
+
+def test_prescriptions_valid_json(client):
+    response = client.post('/prescriptions', data=json.dumps({
+        "clinic": {"id": 1},
+        "physician": {"id": 1},
+        "patient": {"id": 1},
+        "text": "Dipirona 1x ao dia"
+    }))
     assert response.status_code == 200
+
